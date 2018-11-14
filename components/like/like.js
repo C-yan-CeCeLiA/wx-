9eb26c7c6241d4839e460e3e945355b30e85ad69
2b9373669e2749358d40d2c6f2c4a522 <2391301541@qq.com>
2018-11-14 16:34:40 +0800

    first

---
+++ components/like/like.js
@@ -0,0 +1,27 @@
+// components/like/like.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    like:false,
+    count2:99,
+    count1:9,
+    yesSrc:"images/like.png",
+    noSrc:"images/like@dis.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
