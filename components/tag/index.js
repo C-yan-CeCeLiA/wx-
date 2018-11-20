6e9fcae5c01e6f8dff39688ecadf609890fa6211
2b9373669e2749358d40d2c6f2c4a522 <2391301541@qq.com>
2018-11-20 14:16:43 +0800

    实现tag组件

--- components/tag/index.js
+++ components/tag/index.js
@@ -2,22 +2,28 @@
 Component({
   /**
    * 组件的属性列表
+   * 
    */
+  externalClasses: ['tag-class'],
+  options: {
+    multipleSlots:true
+  },
   properties: {
-
+    text:String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    
+     
   }
 })
